e33d536d2fdd35531155e12f5f104fa149c1cd39
TomasHurtado <33264896+TomasHurtado@users.noreply.github.com>
2019-04-25 22:21:21 -0300

    Add files via upload
    int buscarposporid(Pantalla* pantallas,int len,int idabuscar,int* pindex)
    { 
       int i;
    ret=-1;
    for(i=0;i<len;i++)
         {
        if( pantallas[i].idpantalla==idabuscar)
               {
                  *pindex=i;
                  ret=0;
                  break;
                }
         }
    return ret;
    }

---
+++ main.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "string.h"
+#include "pantallas.h"
+#include "publicidad.h"
+#define CANTIPANTALLAS 100
+#define CANTICONTRATACION 1000
+
+int main()
+{
+    char seguir='s'; //MENU
+    int opcion; //MENU
+    int valor;
+    int valores;
+    int posLibre;
+    Pantalla pantallas[CANTIPANTALLAS];
+    Publicidad publicidades[CANTICONTRATACION];
+
+    valor=pant_Inicializar(pantallas, CANTIPANTALLAS);
+    valores=pub_Inicializar(publicidades,CANTICONTRATACION);
+
+    if (valor==0)
+    {
+        printf("Pantalla inicializados correctamente\n\n\n");
+    }
+    if (valores==0)
+    {
+        printf("Pantalla publicidad correctamente\n\n\n");
+    }
+    while (seguir=='s')
+    {
+        printf("\t\tMENU");
+        printf("\n1-Alta Pantalla");
+        printf("\n2-Mostrar Pantalla");
+        printf("\n3-Borrar pantalla");
+        printf("\n4-Alta Publicidad");
+        printf("\n5-Mostrar Publicidad");
+        printf("\n6-Borrar Publicidad");
+        printf("\n7-Salir");
+
+        do
+        {
+            printf("\n\tingrese opcion: ");
+            scanf("\n%d",&opcion);
+        }
+        while (opcion<1 || opcion>7);
+
+        switch (opcion)
+        {
+        case 1:
+
+            if(pant_buscarLibre(pantallas, CANTIPANTALLAS, &posLibre)!=0)
+            {
+                printf("LLENO\n\n");
+            }
+            else
+            {
+                switch (pant_Alta(pantallas, CANTIPANTALLAS, posLibre))
+                {
+                case 0:
+                    printf("dato ingresado correctamente\n\n");
+
+                    break;
+                case 1:
+                    printf("dato ingresado INCORRECTAMENTE\n\n");
+                    break;
+                }
+            }
+            break;
+
+        case 2:
+
+            pant_mostrarArray(pantallas,CANTIPANTALLAS);
+            break;
+
+        case 3:
+            if (pant_baja(pantallas, CANTIPANTALLAS)==0)
+            {
+                printf("exito");
+            }
+            break;
+
+        case 4:
+        {
+        if(pub_buscarLibre(publicidades, CANTICONTRATACION, &posLibre)!=0)
+            {
+                printf("LLENO\n\n");
+            }
+            else
+            {
+                switch (pub_Alta(publicidades, CANTICONTRATACION, posLibre))
+                {
+                case 0:
+                    printf("dato ingresado correctamente\n\n");
+
+                    break;
+                case 1:
+                    printf("dato ingresado INCORRECTAMENTE\n\n");
+                    break;
+                }
+            }
+            break;
+        }
+        case 5:
+        {
+            pub_mostrarArray(publicidades,CANTICONTRATACION);
+            break;
+        }
+
+        case 6:
+        {
+            if (pub_baja(publicidades, CANTICONTRATACION)==0)
+            {
+                printf("exito");
+            }
+            break;
+
+        }
+        case 7:
+            seguir='f';
+            break;
+        }
+    }
+    return 0;
+}
+
+
+//prototipos privados q se usan aca
+//static int generarid(void)
+
+
+
+//empleados[poslibre].id=generarid();
+
+/*
+opcion 2 privado
+static int idmax=0;
+static int generarid(void)
+{
+return idmax++;
+}
+
+opcion 3global
+static int generarid(void)
+{
+static int idmax=0;
+return idmax++;
+}
+*/
