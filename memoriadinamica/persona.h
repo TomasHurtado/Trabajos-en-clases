4b0dce164ab7c79145a8dd2a8feebd598eb1a52c
TomasHurtado <tomas221298@gmail.com>
2019-05-23 22:01:35 -0300

    intento

--- memoriadinamica/persona.h
+++ memoriadinamica/persona.h
@@ -12,12 +12,12 @@ typedef struct
 
 Persona* persona_new();
 Persona* persona_newParametros(int id,char* nombre, char* apellido,int edad);
-//busca
+//escribe
 int persona_setId(Persona* this, int* id);
 int persona_setname(Persona* this, char* nombre);
 int persona_setapellido(Persona* this, char* apellido);
 int persona_setedad(Persona* this, int* edad);
-//asigna
+//lee
 int personas_getId(Persona* this,int* pid);
 int personas_getname(Persona* this, char* pnombre);
 int personas_getapelldio(Persona* this, char* papellido);
