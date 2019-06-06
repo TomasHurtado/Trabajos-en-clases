f1db3c4f33f7958cb403bae1414b5d731fc4f37d
TomasHurtado <33264896+TomasHurtado@users.noreply.github.com>
2019-06-05 22:07:35 -0300

    tp3

---
+++ asd/TP3/Linux_64/Employee.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+#include "utn.h"
+
+
+Employee* employee_new()
+{
+    return (Employee*) malloc(sizeof(Employee));
+}
+
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr,char* sueldo)
+{
+    Employee* emp=employee_new();
+    if(emp!=NULL)
+    {
+        /*empsona_setNombre(emp,nombre);
+        empsona_setApellido(emp,apellido);
+        empsona_setEdad(emp,edad);*/
+    }
+
+
+    return emp;
+}
+
+
+
+int employee_setId(Employee* this,int id)
+{
+    int retorno = -1;
+    if(this != NULL && id >= 0)
+    {
+        this->id = id;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+
+int employee_getId(Employee* this,int* id)
+{
+    int retorno = -1;
+    if(this != NULL)
+    {
+        *id = this->id;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_setNombre(Employee* this,char* nombre)
+{
+
+    int retorno = -1;
+    if(this != NULL && isLetter(nombre))
+    {
+        strncpy(this->nombre,nombre,sizeof(this->nombre));
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_getNombre(Employee* this,char* nombre)
+{
+    int retorno = -1;
+    if(this != NULL)
+    {
+        strncpy(nombre,this->nombre,sizeof(this->nombre));
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas)
+{
+    int retorno = -1;
+    if(this != NULL && horasTrabajadas >= 0)
+    {
+        this->horasTrabajadas = horasTrabajadas;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas)
+{
+    int retorno = -1;
+    if(this != NULL)
+    {
+        *horasTrabajadas = this->horasTrabajadas;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_setSueldo(Employee* this,int sueldo)
+{
+
+    int retorno = -1;
+    if(this != NULL && sueldo >= 0)
+    {
+        this->sueldo = sueldo;
+        retorno = 0;
+    }
+    return retorno;
+}
+
+int employee_getSueldo(Employee* this,int* sueldo)
+{
+     int retorno = -1;
+    if(this != NULL)
+    {
+        *sueldo = this->sueldo;
+        retorno = 0;
+    }
+    return retorno;
+}
